3d2e80e1179711c32272be93351932d1b2b70941
Ojiteli Kosisochukwu <obuekwekosi@gmail.com>
2025-07-25 09:51:05 +0100

    fix

--- src/app/api/paystack/route.js
+++ src/app/api/paystack/route.js
@@ -11,7 +11,7 @@ export async function POST(request) {
       {
         email,
         amount ,
-        callback_url : 'http://localhost:3000/'
+        callback_url : 'https://fun-verge.vercel.app/'
       },
       {
         headers: {
